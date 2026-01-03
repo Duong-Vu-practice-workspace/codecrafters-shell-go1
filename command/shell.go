a90893de3030fa7fc3e9d06fab8801adcc6f0769
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-01-03 15:53:31 +0700

    Run a program #ip1

--- command/shell.go
+++ command/shell.go
@@ -10,12 +10,13 @@ func DefaultShellCommand(cmd string, args []string) {
 	fmt.Printf("%s: command not found\n", cmd)
 }
 func runCommand(cmd string, args []string) {
-	_, ok := searchPath(cmd)
+	path, ok := searchPath(cmd)
 	if !ok {
 		fmt.Printf("%s: not found\n", cmd)
 		return
 	}
 	runCmd := exec.Command(cmd, args...)
+	runCmd.Path = path
 	runCmd.Stdin = os.Stdin
 	runCmd.Stdout = os.Stdout
 	runCmd.Stderr = os.Stderr
