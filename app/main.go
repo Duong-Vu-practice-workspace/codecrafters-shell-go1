e554fc449c76bf5cab8dd0eb4d18eb209c384baf
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-01-03 11:13:57 +0700

    Implement type #ez5

--- app/main.go
+++ app/main.go
@@ -11,25 +11,44 @@ import (
 var _ = fmt.Print
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		fmt.Print("$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		command, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		trimmedCommand := command[:len(command)-1]
-		//fmt.Println("command:" + command + ".")
-		params := strings.Split(trimmedCommand, " ")
+
+		trimmedCommand := strings.TrimSpace(command)
+		if trimmedCommand == "" {
+			continue
+		}
+
+		params := strings.Fields(trimmedCommand)
+		if len(params) == 0 {
+			continue
+		}
+
 		switch params[0] {
 		case "exit":
 			os.Exit(0)
 		case "echo":
 			fmt.Println(strings.Join(params[1:], " "))
+		case "type":
+			if len(params) != 2 {
+				fmt.Fprintln(os.Stderr, "Invalid param passed")
+				os.Exit(1)
+			}
+			param := params[1]
+			if param == "exit" || param == "echo" || param == "type" {
+				fmt.Printf("%s is a shell builtin\n", param)
+			} else {
+				fmt.Printf("%s: not found\n", param)
+			}
 		default:
 			fmt.Printf("%s: command not found\n", trimmedCommand)
 		}
-
 	}
-
 }
